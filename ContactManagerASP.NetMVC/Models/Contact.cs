83715088cad8d8b03eec1c083e725ec2d9fe0aac
tmaPeniel <taouemap@gmail.com>
2024-11-04 13:40:07 +0100

    Dépot initial

---
+++ ContactManagerASP.NetMVC/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManagerASP.NetMVC.Models
+{
+    public class Contact
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage ="Format incorect. Veuillez ressaisir")]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone(ErrorMessage ="Format incorrect. Veuillez réessayer")]
+        [StringLength(10, ErrorMessage ="Veuillez saisir 10 chiffres")]
+        public string Numero { get; set; }
+    }
+}
