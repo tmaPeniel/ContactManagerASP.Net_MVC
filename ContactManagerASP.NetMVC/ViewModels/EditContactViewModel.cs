5acca47ffcfba9d05b2bc351a9fb9e0a994922f6
tmaPeniel <taouemap@gmail.com>
2024-11-08 09:44:38 +0100

    Fonction Update et creation d'un view modele pour la vue de modification

---
+++ ContactManagerASP.NetMVC/ViewModels/EditContactViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ContactManagerASP.NetMVC.ViewModels
+{
+    public class EditContactViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+        public string Numero { get; set; }
+
+    }
+}
