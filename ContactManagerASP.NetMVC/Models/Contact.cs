9a1fd1536cc57e11479ba2a755e97983f22b1391
tmaPeniel <taouemap@gmail.com>
2024-11-08 15:51:50 +0100

    Fonctionnalité de recherche et sécurité de l'application : Saisie sécurisée du numero (10 chiffres), le mail doit respecter le foramt username@nomde.domaine

--- ContactManagerASP.NetMVC/Models/Contact.cs
+++ ContactManagerASP.NetMVC/Models/Contact.cs
@@ -5,12 +5,20 @@ namespace ContactManagerASP.NetMVC.Models
     public class Contact
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
 
         public string Surname { get; set; }
 
+        [Required]
+        [EmailAddress(ErrorMessage = "L'adresse e-mail n'est pas valide.")]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "L'adresse e-mail n'est pas valide.")]  //Permet de verifier la struture "username@domaine.com"
         public string Email { get; set; }
-        
+
+        [Required]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Le numéro de téléphone doit comporter 10 chiffres.")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Le numéro de téléphone doit être exactement de 10 chiffres.")]
         public string Numero { get; set; }
     }
 }
